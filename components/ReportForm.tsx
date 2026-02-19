5b1f43bc413bda30b2adb45f7702a9f93eaf1029
Maxime Lust <maxime@favoreatdesign.com>
2026-02-19 11:56:32 +0100

    feat: update ReportForm with LocationButton

--- components/ReportForm.tsx
+++ components/ReportForm.tsx
@@ -1,37 +1,41 @@
-'use client';
+"use client";
 
-import PrimaryButton from './ui/PrimaryButton';
-import { createClient, withUser } from '@/lib/supabase/client';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import TextAreaWithLengthIndicator from './form/TextAreaWithLengthIndicator';
+import PrimaryButton from "@/components/ui/PrimaryButton";
+import LocationButton from "@/components/form/LocationButton";
+import TextAreaWithLengthIndicator from "@/components/form/TextAreaWithLengthIndicator";
+import { createClient, withUser } from "@/lib/supabase/client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const ReportForm = () => {
   const router = useRouter();
   const supabase = createClient();
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     await withUser(supabase, router, async (user) => {
-      const { error } = await supabase.from('reports').insert({ author_id: user.id, description });
+      const { error } = await supabase
+        .from("reports")
+        .insert({ author_id: user.id, description });
       if (error) throw error;
 
-      router.push('/form-submitted');
+      router.push("/form-submitted");
     });
   };
 
   return (
     <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+      <LocationButton />
       <TextAreaWithLengthIndicator
         label="Description"
         value={description}
         maxLength={250}
-        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setDescription(e.target.value)}
+        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+          setDescription(e.target.value)
+        }
       />
-      <PrimaryButton type="submit">
-        Je valide mon signalement
-      </PrimaryButton>
+      <PrimaryButton type="submit">Je valide mon signalement</PrimaryButton>
     </form>
   );
 };
