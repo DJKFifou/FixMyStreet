5f171a1153d19d375ff5843d7b3b72686958c0a9
Samuel-Labagnere <samuel.labagnere@gmail.com>
2026-02-19 10:21:05 +0100

    fix: Added spacing

--- app/(form)/form-submitted/page.tsx
+++ app/(form)/form-submitted/page.tsx
@@ -4,12 +4,12 @@ import SecondaryLink from "@/components/ui/SecondaryLink";
 const Page = () => {
   return(
     <div className="grow flex flex-col items-center justify-center">
-      <div className="grow flex flex-col items-center justify-center w-3/4">
-        <h1 className="text-2xl text-center mb-4">Merci pour votre signalement !</h1>
+      <div className="grow flex flex-col gap-4 items-center justify-center w-3/4">
+        <h1 className="text-2xl text-center">Merci pour votre signalement !</h1>
         <small className="text-sm text-center text-gray-500">
           Nous vous remercions de votre contribution.
         </small>
-        <Redirector href="/" time={5} additionalClasses="text-center text-gray-500" />
+        <Redirector href="/" time={20} additionalClasses="text-center text-gray-500" />
       </div>
       <SecondaryLink href="/">Retour à l&apos;accueil</SecondaryLink>
     </div>
