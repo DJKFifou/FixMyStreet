f67f2416aac56430c64b0a9631bf579d73b64d41
Samuel-Labagnere <samuel.labagnere@gmail.com>
2026-02-20 11:49:41 +0100

    feat: Added a states mapper

---
+++ lib/utils/states.ts
@@ -0,0 +1,11 @@
+/*
+* [SL] States mapping for the report status
+* Usage : states[report.status]
+*/
+export const states = {
+  created: "Créé",
+  validated: "Validé",
+  in_charge: "Pris en charge",
+  done: "Terminé",
+  rejected: "Rejeté"
+};
