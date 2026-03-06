d2f3231e98710123e5fd56b4dce9f970d55bd887
Samuel-Labagnere <samuel.labagnere@gmail.com>
2026-03-06 10:28:54 +0100

    style: Proper title

--- app/user/reports/page.tsx
+++ app/user/reports/page.tsx
@@ -4,7 +4,9 @@ const ReportsPage = () => {
   return (
     <div>
       <BackLinkHeader href="/" title="Historique des signalements" />
-      <h1>Signalements</h1>
+      <div className="mt-12">
+        <h1 className="w-full text-center text-3xl leading-10">Mes signalements</h1>
+      </div>
     </div>
   );
 }
